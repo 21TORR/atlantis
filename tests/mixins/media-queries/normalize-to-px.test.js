9b392f817aa24cf9b70af93c3ee2da6754e4f394
Jannik Zschiesche <j.zschiesche@21torr.com>
2020-11-04 17:08:36 +0100

    Add base mixins: media-queries

---
+++ tests/mixins/media-queries/normalize-to-px.test.js
@@ -0,0 +1,46 @@
+import test from "ava";
+import path from "path";
+import sass from "sass";
+import {compileScss} from "../../helpers/scss";
+
+
+test("normalize-to-px: Normalize to px variations", t => {
+	[
+		["1.1rem", "11px"],
+		["11px", "11px"],
+	].forEach(([input, expected]) => {
+		const css = compileScss(`
+			@use "mixins/media-queries";
+			
+			a {
+				content: media-queries.normalize-to-px(${input});
+			}
+		`);
+
+		t.is(css, `a{content:${expected}}`);
+	});
+});
+
+
+test("normalize-to-px: Invalid units", t => {
+	[
+		"in",
+		"cm",
+		"mm",
+		"pt",
+		"em",
+		"",
+	].forEach(unit => {
+		const error = t.throws(
+			() => compileScss(`
+				@use "mixins/media-queries";
+				
+				a {
+					content: media-queries.normalize-to-px(11${unit});
+				}
+			`),
+		);
+
+		t.regex(error.message, new RegExp(`Can't automatically convert value with unit '${unit}'`));
+	});
+});
---
+++ tests/private-functions-hidden.test.js
@@ -0,0 +1,23 @@
+import test from "ava";
+import {compileScss} from "./helpers/scss";
+
+
+test("All private functions are not visible", t => {
+	const privateFunctions = [
+		"normalize-to-px(10rem)",
+	];
+
+	privateFunctions.forEach(functionCall => {
+		const error = t.throws(
+			() => compileScss(`
+			@use "mixins";
+			
+			.my-selector {
+				content: mixins.${functionCall};
+			}
+			`),
+		);
+
+		t.regex(error.message, /Undefined function/);
+	});
+});
